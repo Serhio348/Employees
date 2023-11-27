a47ef3b220e31d99050e6aca7068922468c18661
Serhio348 <serhiosidorovich@gmail.com>
2023-11-27 20:18:59 +0300

    add register form

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Layout from "../../components/layout/Layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import LoginInput from "../../components/loginInput/LoginInput";
+import PasswordInput from "../../components/passwordInput/PasswordInput";
+import CustomButton from "../../components/customButton/CustomButton";
+import { Link } from "react-router-dom";
+import { Paths } from "../../path";
 
 const Register = () => {
     return (
-        <div>Register</div>
-    )
-}
+        <Layout>
+            <Row align="middle" justify="center">
+                <Card title="Регистрация" style={{ width: "30rem" }}>
+                    <Form>
+                        <LoginInput name="name" placeholder="Имя" />
+                        <LoginInput type="email" name="email" placeholder="Email" />
+                        <PasswordInput name="password" placeholder="Пароль" />
+                        <PasswordInput name="confirmPassword" placeholder="Повторите пароль" />
+                        <CustomButton type="primary" htmlType="submit">
+                            Зарегистрироваться
+                        </CustomButton>
+                    </Form>
+                    <Space direction="vertical" size="large">
+                        <Typography.Text>
+                            Уже зарегестрированы? <Link to={Paths.login}>Войдите</Link>
+                        </Typography.Text>
+                    </Space>
+                </Card>
+            </Row>
+        </Layout>
+    );
+};
 
-export default Register
+export default Register;
